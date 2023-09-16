737b70cd673dca30dc5087c63e6e0b0b22d1970c
masayuki <b.maca217@gmail.com>
2023-09-16 15:44:30 +0900

    uniswapV2: Type testing

--- scripts/uniswapV2.ts
+++ scripts/uniswapV2.ts
@@ -7,7 +7,7 @@ import { abi as IUniswapV2FactoryABI } from '@uniswap/v2-core/build/IUniswapV2Fa
 import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 
 const chainId = 1;
-const FACTORY_ADDRESS = '0xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' //UniswapV2Factoryのコントラクトアドレス
+const FACTORY_ADDRESS = '0x00000000000000000000000000000000000' //UniswapV2Factoryのコントラクトアドレス
 const tokens: TokenData[] = _tokens;
 
 interface TokenData {
@@ -40,14 +40,35 @@ async function main(tokenSymbolA: string, tokenSymbolB: string) {
      * 読んでる途中どっちがどっちだっけってならずに済む。
      * シンボルを変数で包むとどっちがどっちだってなる　＝＞　大小でソートして変数にも数字で大小付ければ見やすい
     */
+
+    /**
+     * BigIntはJavascriptのもの。9,007,199,254,740,991(253 – 1)より大きい値を扱う時に使用する。
+     * BigNumberはethersのもの。小数が切り取られた整数。
+     * 多分,,,。
+     */
     console.log(reserve0);
     console.log(typeof reserve0);
-    const denominator = BigInt(reserve0) * (10n ** BigInt(token1.decimals));
-    const numerator = BigInt(reserve1) * (10n ** BigInt(token0.decimals));
+    const denominator = BigInt(reserve0) * (10n ** BigInt(token1.decimals)); //(※1)
+    const numerator = BigInt(reserve1) * (10n ** BigInt(token0.decimals)); //(※2)
     const precision = 15;
     const price = Number(10n ** BigInt(precision) * numerator / denominator) / (10 ** precision);
     console.log(`1 ${token0.symbol} = ${price} ${token1.symbol}`)
     console.log(`1 ${token1.symbol} = ${1 / price} ${token0.symbol}`)
+    /**
+     * (※1)分子のreserve1の値をsolidity用の桁が多いものから人用の値にするためにdecimalsの分だけ10で割ってるだけ。
+     * (※2)分母のreserve0の値をsolidity用の桁が多いものから人用の値にするためにdecimalsの分だけ10で割ってるだけ。
+     */
+
+    //上のやつの型実験。内容も結果も同じ。
+    // console.log(reserve0);
+    // console.log(typeof reserve0);
+    // const denominator_ = BigInt(reserve0) * (10n ** BigInt(token1.decimals));
+    // const numerator_ = BigInt(reserve1) * (10n ** BigInt(token0.decimals));
+    // const denominator = Number(denominator_);
+    // const numerator = Number(numerator_);
+    // const price = numerator / denominator;
+    // console.log(`1 ${token0.symbol} = ${price} ${token1.symbol}`)
+    // console.log(`1 ${token1.symbol} = ${1 / price} ${token0.symbol}`)
 }
 
 program
